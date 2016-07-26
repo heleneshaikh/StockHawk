277ecf3b27b9cf57ea1b5992d8d2e77f0e3ba33f
heleneshaikh <helene.shaikh@gmail.com>
2016-07-26 14:10:12 +0200

    add stock widget provider

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetProvider.java
@@ -0,0 +1,94 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.RemoteViews;
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.rest.StockTaskService;
+import com.sam_chordas.android.stockhawk.ui.DetailActivity;
+import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class StockWidgetProvider extends AppWidgetProvider {
+    public static final String DETAIL_ACTION = "com.sam_chordas.android.stockhawk.DETAIL_ACTION";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_widget);
+
+            Intent intent = new Intent(context, MyStocksActivity.class); //not stock service?
+            //pendingIntent => widget
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget_title, pendingIntent);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                setRemoteAdapter(context, views);
+            } else {
+                setRemoteAdapterV11(context, views);
+            }
+
+            Intent intentTemplate = new Intent(context, DetailActivity.class);
+            intentTemplate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+            intentTemplate.setAction(DETAIL_ACTION);
+            PendingIntent pendingIntentTemplate = android.support.v4.app.TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(intentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.widget_list, pendingIntentTemplate);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widget_list);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (StockTaskService.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list);
+        }
+        if(intent.getAction().equals(StockWidgetProvider.DETAIL_ACTION)){
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            context.startActivity(intent);
+        }
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, StockWidgetService.class));
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @SuppressWarnings("deprecation")
+    private void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list,
+                new Intent(context, StockWidgetService.class));
+    }
+}
+
---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/StockWidgetService.java
@@ -0,0 +1,7 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+/**
+ * Created by heleneshaikh on 26/07/16.
+ */
+public class StockWidgetService {
+}
